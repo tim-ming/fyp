6d1166c8a4158089c75c0defe5f0aba0d3ce93bb
ming <timming.au@gmail.com>
2024-08-08 16:31:03 +0800

    add back index

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold mb-4">HELLO</Text>
+    </View>
+  );
+};
+
+export default IndexScreen;

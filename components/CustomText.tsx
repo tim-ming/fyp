68e43bbe08eafdf31670391521662c5a61913214
ming <timming.au@gmail.com>
2024-08-19 00:11:40 +0800

    add letter spacing variant

--- components/CustomText.tsx
+++ components/CustomText.tsx
@@ -4,15 +4,19 @@ import { Text, StyleSheet, TextProps, TextStyle } from "react-native";
 interface CustomTextProps extends TextProps {
   children: ReactNode;
   style?: TextStyle | TextStyle[];
+  letterSpacing?: "tighter" | "tight" | "normal" | "wide";
 }
 
 const CustomText: React.FC<CustomTextProps> = ({
   children,
   style,
+  letterSpacing = "normal",
   ...props
 }) => {
+  const letterSpacingStyle = styles[letterSpacing];
+
   return (
-    <Text style={[styles.text, style]} {...props}>
+    <Text style={[styles.text, letterSpacingStyle, style]} {...props}>
       {children}
     </Text>
   );
@@ -21,6 +25,18 @@ const CustomText: React.FC<CustomTextProps> = ({
 const styles = StyleSheet.create({
   text: {
     fontFamily: "PlusJakartaSans",
+  },
+  tighter: {
+    letterSpacing: -0.75,
+  },
+  tight: {
+    letterSpacing: -0.5,
+  },
+  normal: {
+    letterSpacing: -0.25,
+  },
+  wide: {
+    letterSpacing: 0.5,
   },
 });
 
